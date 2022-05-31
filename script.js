035940a77d01b8848ad343307511ae9b9e724a21
mohamad-gandomi <m.gandomi1995@gmail.com>
2022-05-31 15:58:44 +0400

    refactor some function

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-//jQuery time
 var animating; //flag to prevent quick multi-click glitches
-var no_validate = false;
 
-const goToNextFieldset = (current_fs, next_fs, duration = 500) => {
+const validateFieldset = () => {
+	let elements = [];
+	let errors = [];
+	elements = $('input,textarea,select').filter('[required]:visible');
+	elements.each(function() {
+		if (!$(this).val()) {
+			errors.push('required');
+		}
+	});
+
+	if (errors.length) {
+		return false;
+	} else {
+		$('button').filter('[disabled]:visible').removeAttr('disabled');
+		return true;
+	}
+
+}
+
+const goToNextFieldset = (next_fs, duration = 500) => {
+
+	if (!validateFieldset()) {
+		animating = false;
+		return false;
+	}
+	
+	let current_fs = $('fieldset').filter(':visible');
+	next_fs.find('.previous').attr('data-btn', current_fs.attr('data-fs'));
+	current_fs.find('.next').attr('data-btn', next_fs.attr('data-fs'));
+
 	next_fs.show();
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
@@ -21,7 +48,9 @@ const goToNextFieldset = (current_fs, next_fs, duration = 500) => {
 	});
 }
 
-const goToPreviousFieldset = (current_fs, previous_fs, duration = 500) => {
+const goToPreviousFieldset = (previous_fs, duration = 500) => {
+
+	let current_fs = $('fieldset').filter(':visible');
 	previous_fs.show();
 	current_fs.animate({opacity: 0}, {
 		step: function(now, mx) {
@@ -42,111 +71,50 @@ const goToPreviousFieldset = (current_fs, previous_fs, duration = 500) => {
 
 $('.select2').select2();
 
-$(".input-btn").click(function(e){
-	e.preventDefault();
-	if(animating) return false;
-	animating = true;
 
-	$('.input-btn').removeClass('selected');
-	$(this).addClass('selected');
-	$(this).parent().find('.next').removeAttr("disabled");
-	current_fs = $(this).parent();
-	next_fs = $( 'fieldset[data-fs='+ $(this).attr('data-btn') + ']' ); 
-
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-	//add this fieldset data-fs to next_fs previous button data-btn
-	next_fs.find('.previous').attr('data-btn', current_fs.attr('data-fs'));
-	current_fs.find('.next').attr('data-btn', next_fs.attr('data-fs'));
-	
-	goToNextFieldset (current_fs, next_fs, 500);
-
-});
-
-$(".next").click(function(e){
-
-	e.preventDefault();
-
-	if(animating) return false;
-
-	$(this).parent().parent().find('.required').each(function(){
-		if (!$(this).val()) {
-			no_validate = true;
-		} else {
-			no_validate = false;
-		}
-	});
-
-	if (no_validate) return;
-
-	current_fs = $(this).parent().parent();
-	next_fs = $( 'fieldset[data-fs='+ $(this).attr('data-btn') + ']' );
-	if(!next_fs.length) next_fs = $(this).parent().parent().next();
-
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	goToNextFieldset (current_fs, next_fs, 500);
-});
 
 $(".previous").click(function(e){
 
 	e.preventDefault();
-
 	if(animating) return false;
 	animating = true;
-	
-	current_fs = $(this).parent().parent();
 	previous_fs = $( 'fieldset[data-fs='+ $(this).attr('data-btn') + ']' );
 	if(!previous_fs.length) previous_fs = $(this).parent().parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-	goToPreviousFieldset (current_fs, previous_fs, 500);
+	goToPreviousFieldset (previous_fs, 500);
 	
 });
 
 $('select').on('change', function() {
 
 	if(animating) return false;
-
-	$(this).parent().parent().find('.required').each(function(){
-		if(!$(this).val()){
-			no_validate = true;
-			$(this).parent().parent().find('.next').attr('disabled', 'disabled');
-			$(this).parent().parent().find('.next').removeAttr('data-btn');
-			return;
-		} else {
-			no_validate = false;
-		}
-	});
-
 	if($('.select2').find(':selected').length) {
 		$(this).next().addClass('has-option');
 	} else {
 		$(this).next().removeClass('has-option');
 	}
+	animating = true;
+	next_fs = $( 'fieldset[data-fs='+ $(this).find(':selected').attr('data-option') + ']' );
+	goToNextFieldset (next_fs, 500);
 
-	if (no_validate) return;
+});
 
+$(document).on('click change', '.next, input[data-btn], select', function(e){
+
+	e.preventDefault();
+
+	if(animating) return false;
 	animating = true;
 
+	next_fs = $( 'fieldset[data-fs='+ $(this).attr('data-btn') + ']' );
 
-	$(this).parent().parent().find('.next').removeAttr("disabled");
-	
-	current_fs = $(this).parent().parent();
-	next_fs = $( 'fieldset[data-fs='+ $(this).find(':selected').attr('data-option') + ']' );
-	if(!next_fs.length) next_fs = $(this).parent().parent().next();
+	goToNextFieldset (next_fs, 500);
 
-	next_fs.find('.previous').attr('data-btn', current_fs.attr('data-fs'));
-	current_fs.find('.next').attr('data-btn', next_fs.attr('data-fs'));
+})
 
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	goToNextFieldset (current_fs, next_fs, 500);
-});
+
+// $('.input-btn').removeClass('selected');
+// $(this).addClass('selected');
+
+$("form :input").change(function() {
+	$(this).addClass('test');
+})
